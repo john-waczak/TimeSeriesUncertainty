d9efe981d04cff3fb7a7f547532bde05e39dc6f1
John Waczak <jwaczak@login-01.localdomain>
2023-11-16 09:18:12 -0600

    added clone file

---
+++ clone-data.sh
@@ -0,0 +1,20 @@
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_1 /scratch/jwaczak/data/aq-data/raw/IPS7100 --include "*IPS7100*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_1 /scratch/jwaczak/data/aq-data/raw/BME680 --include "*BME680*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_1 /scratch/jwaczak/data/aq-data/raw/BME280 --include "*BME280*.csv" -P
+
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_4 /scratch/jwaczak/data/aq-data/raw/IPS7100 --include "*IPS7100*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_4 /scratch/jwaczak/data/aq-data/raw/BME680 --include "*BME680*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_4 /scratch/jwaczak/data/aq-data/raw/BME280 --include "*BME280*.csv" -P
+
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_10 /scratch/jwaczak/data/aq-data/raw/IPS7100 --include "*IPS7100*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_10 /scratch/jwaczak/data/aq-data/raw/BME680 --include "*BME680*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_10 /scratch/jwaczak/data/aq-data/raw/BME280 --include "*BME280*.csv" -P
+
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_8 /scratch/jwaczak/data/aq-data/raw/IPS7100 --include "*IPS7100*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_8 /scratch/jwaczak/data/aq-data/raw/BME680 --include "*BME680*.csv" -P
+rclone copyto OSN:ees230012-bucket01/AirQualityNetwork/data/raw/Central_Hub_8 /scratch/jwaczak/data/aq-data/raw/BME280 --include "*BME280*.csv" -P
+
+
+
+
+
